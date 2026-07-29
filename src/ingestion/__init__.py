29a5dc11f04ca634888ff7b883d7142031f53a23
IDontFeelNone <58sh8mt7p2@privaterelay.appleid.com>
2026-07-28 20:05:37 -0400

    Add ingestion package initializer

---
+++ src/ingestion/__init__.py
@@ -0,0 +1,7 @@
+"""MTG Lab ingestion package.
+
+This package contains data ingestion pipelines responsible for importing,
+normalizing, and validating data from official and community sources.
+"""
+
+__all__ = []
